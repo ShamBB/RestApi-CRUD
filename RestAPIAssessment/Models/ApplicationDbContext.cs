300022ba58c22d52e46eaa8bc25c7bdb91a4e6ff
shamBB <sham_berani@yahoo.com>
2024-09-06 09:27:46 +0800

    first commit

---
+++ RestAPIAssessment/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace RestAPIAssessment.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional configurations if needed
+        }
+    }
+}
